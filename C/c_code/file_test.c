5c7e21c9b9943fa169a8bf3621d37d62b4a8fd1d
more1than <zh270193053@sina.com>
2022-11-20 15:16:29 +0800

    (2022/11/20)预编译与文件系统

---
+++ C/c_code/file_test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // // 打开文件
+    // FILE *pf = fopen("test.txt", "r");
+    // fclose(pf);
+    // pf = NULL;
+    // return 0;
+
+    return 0;
+}
--- C/c_code/point.c
+++ C/c_code/point.c
@@ -1,40 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include <assert.h>
 
-struct S
-{
-    int a;
-    int arr[]; // 柔性数组成员
-};
+// struct S
+// {
+//     int a;
+//     int arr[]; // 柔性数组成员
+// };
 
-int main()
-{
-    // struct S s;
-    // printf("%d\n", sizeof(s));
-    struct S *ps = (struct S *)malloc(sizeof(struct S) + 5 * sizeof(int));
-    ps->a = 100;
+// int main()
+// {
+//     // struct S s;
+//     // printf("%d\n", sizeof(s));
+//     struct S *ps = (struct S *)malloc(sizeof(struct S) + 5 * sizeof(int));
+//     ps->a = 100;
 
-    int i = 0;
-    for (i = 0; i < 5; i++)
-    {
-        ps->arr[i] = i;
-    }
-    struct S *ptr = realloc(ps, 44);
-    if (ptr != NULL)
-    {
-        ps = ptr;
-    }
-    for (i = 5; i < 10; i++)
-    {
-        printf("%d\n", ps->arr[i]);
-    }
-    // 释放动态内存
-    free(ps);
-    ps = NULL;
-    return 0;
-}
+//     int i = 0;
+//     for (i = 0; i < 5; i++)
+//     {
+//         ps->arr[i] = i;
+//     }
+//     struct S *ptr = realloc(ps, 44);
+//     if (ptr != NULL)
+//     {
+//         ps = ptr;
+//     }
+//     for (i = 5; i < 10; i++)
+//     {
+//         printf("%d\n", ps->arr[i]);
+//     }
+//     // 释放动态内存
+//     free(ps);
+//     ps = NULL;
+//     return 0;
+// }
 
 // char pp(void)
 // {
